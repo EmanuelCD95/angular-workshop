975042068a7e7d8114c86c60a6faa5e138f33bcc
Emanuel Duarte <emanuel.duarte@innowave.tech>
2018-10-03 11:47:57 +0100

    step 7

--- src/state/state.module.ts
+++ src/state/state.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { counterReducer } from '@state/counter/counter.reducer';
 
@@ -7,7 +8,8 @@ import { counterReducer } from '@state/counter/counter.reducer';
     imports: [
         StoreModule.forRoot({
             count: counterReducer
-        })
+        }),
+        StoreDevtoolsModule.instrument()
     ]
 })
 export class StateModule { }
